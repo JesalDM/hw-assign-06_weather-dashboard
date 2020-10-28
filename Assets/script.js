d347ab587ad1b6d2523e159e5e4597f68764bdea
Jesal Mehta <jesaldmehta@gmail.com>
2020-10-28 16:18:59 -0400

    Registered and used the API key to make the first ajax call request to get the current weather data, mapped the required keys from the response and displayed it on the UI

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 //Register and get the openWeather APIKey
 
+const APIKey = "fa2f3597f83a06c3c808a388a5613793";
+
 /*On page load*/
 //1. Display the weather details of the last searched city
     //1A Retrieve the searched history list from local storage
@@ -17,11 +19,19 @@
 
 
 /*On search button click or when user clicks a city name from teh search history list*/
-//2. Add eventListener to the search button when clicked, to display the corresponsing weather details for the user searched city
-
-    //2A Grab the value of the user entered city name input
+//2. Adds eventListener to the search button when clicked, to display the corresponsing weather details for the user searched city
+$('#searchBtn').on("click", function(event){
+    event.preventDefault();
+    //2A Grabs the value of the user entered city name input
+    const searchedCity = $('#inputSearch').val().trim();
     
-    //2B Send an ajax call to get the current and forecast weather data for that city using 'APIKey' and 'city' parameters
+    //2B Send an ajax request call to get the current and forecast weather data for that city using 'APIKey' and 'city' parameters
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIKey + "&units=imperial";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response); 
 
     //2C Add the city to the searched history list in the local storage. Validate if it already exists and logic to handle accordingly
 
@@ -29,8 +39,24 @@
 
     //2E Display the ajax response data received for current and forecast weather on the UI
             // Map the keys of the required data from the response to the appropriate HTML element for current and forecast weather
+            
+            // creates an image tag for the icon
+            const icon = $('<img>');
+            // adds the src attribute to the img tag using open weather specified format to get icon URL
+            icon.attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png")   
+            // concatenates the city name from the response and current date using moment.js
+            $('.cityName').html("<h3 id='cityDate'>" + response.name + " (" + moment().format('L') + ") " + "</h3>");
+            // appends the icon to the h3 tag alongside city name and date
+            $('.cityName').append(icon);
+            // maps the temperature, humidity and wind speed from the response and displays it on the UI
+            $('.temp').text("Temperature: " + response.main.temp + " °F");
+            $('.humidity').text("Humidity: " + response.main.humidity + " %");
+            $('.wind').text("Wind Speed: " + response.wind.speed + " MPH");
+    });
+
+            // Send another ajax request call to get the UVIndex
             // Include the logic to color code the UV index based on value
             // Create a loop to display the 5-day forecast weather
-
+});
 
 /* Since almost the entire code is repeated on page load and when user clicks on button, code refactoring will be done at the end.*/
